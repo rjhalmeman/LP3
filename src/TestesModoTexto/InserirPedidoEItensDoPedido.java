ee7f9708d0f260724e39aa5583fc77713bf8c225
Radames J Halmeman <rjhalmeman@gmail.com>
2019-08-13 09:18:34 -0300

    LojaZero V3 - CRUD em tabelas com relacionamentos de NxM.

---
+++ src/TestesModoTexto/InserirPedidoEItensDoPedido.java
@@ -0,0 +1,39 @@
+package TestesModoTexto;
+
+import DAOs.DAOCliente;
+import DAOs.DAOFuncionario;
+import DAOs.DAOPedido;
+import Entidades.Pedido;
+import java.util.Date;
+
+/**
+ * @author Radames J Halmeman  - rjhalmeman@gmail.com
+ */
+public class InserirPedidoEItensDoPedido {
+    public static void main(String[] args) {
+        DAOPedido daoPedido = new DAOPedido();
+        Pedido pedido;
+        
+        pedido = new Pedido(1, new Date());
+        if (daoPedido.obter(pedido.getIdPedido())==null) {            
+            pedido.setClientePessoaCpfPessoa(new DAOCliente().obter("4"));
+            pedido.setFuncionarioPessoaCpfPessoa(new DAOFuncionario().obter("2"));
+            daoPedido.inserir(pedido);
+        }
+       
+        pedido = new Pedido(2, new Date());
+        if (daoPedido.obter(pedido.getIdPedido())==null) {            
+            pedido.setClientePessoaCpfPessoa(new DAOCliente().obter("5"));
+            pedido.setFuncionarioPessoaCpfPessoa(new DAOFuncionario().obter("3"));
+            daoPedido.inserir(pedido);
+        }
+        System.out.println("Pedidos inseridos");
+        
+        
+        
+        
+
+
+    }
+
+}
