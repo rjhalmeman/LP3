fd1168f0ba9a5566847b148d539cef10e0f09903
radames <radames@radames-samsung>
2019-11-07 15:44:04 -0200

    CrudCanguru versão 7 - Larissa - Janela pesquisar

---
+++ src/Main/Main.java
@@ -0,0 +1,22 @@
+package Main;
+
+/**
+ *
+ * @author radames
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        
+       
+        
+        
+
+        GUI gui = new GUI();
+    }
+
+}
